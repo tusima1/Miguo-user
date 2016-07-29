71763dd0571e21f2b49a103b32fce1030f587980
cq <2639117470@qq.com>
2016-07-29 19:02:38 +0800

    add 我代言的店铺

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/seller/views/fragment/FragmentMineShopList.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/seller/views/fragment/FragmentMineShopList.java
@@ -2,37 +2,98 @@ package com.fanwe.seller.views.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.fanwe.adapter.MerchantListAdapter;
+import com.fanwe.fragment.BaseFragment;
+import com.fanwe.model.StoreModel;
 import com.fanwe.o2o.miguo.R;
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.lidroid.xutils.view.annotation.ViewInject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 我代言的店铺
  * Created by Administrator on 2016/7/29.
  */
-public class FragmentMineShopList extends Fragment {
-    View view;
+public class FragmentMineShopList extends BaseFragment {
+    private View view;
+    private MerchantListAdapter mAdapter = null;
+    private List<StoreModel> mListModel = new ArrayList<StoreModel>();
 
-    @Nullable
+    @ViewInject(R.id.ptr_listview_fragment_mine_shop_list)
+    private PullToRefreshListView mPtrlvContent = null;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        if (view == null) {
-            initView(inflater, container);
-        }
-        // 缓存的rootView需要判断是否已经被加过parent，
-        // 如果有parent需要从mView删除，要不然会发生这个mView已经有parent的错误。
-        ViewGroup parent = (ViewGroup) view.getParent();
-        if (parent != null) {
-            parent.removeView(view);
-        }
-        return view;
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
-    private void initView(LayoutInflater inflater, ViewGroup container) {
-        view = (View) inflater.inflate(R.layout.fragment_mine_shop_list, container, false);
 
+    @Override
+    protected View onCreateContentView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return setContentView(R.layout.fragment_mine_shop_list);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        preParam();
+        bindDefaultLvData();
+        initPullRefreshLv();
+    }
+
+    private void preParam() {
+        for (int i = 0; i < 10; i++) {
+            StoreModel storeModel = new StoreModel();
+            mListModel.add(storeModel);
+        }
+    }
+
+    private void initPullRefreshLv() {
+        mPtrlvContent.setMode(PullToRefreshBase.Mode.BOTH);
+        mPtrlvContent.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener2<ListView>() {
+
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+
+                mListModel.clear();
+                for (int i = 0; i < 5; i++) {
+                    StoreModel storeModel = new StoreModel();
+                    mListModel.add(storeModel);
+                }
+                mAdapter.notifyDataSetChanged();
+
+                mPtrlvContent.onRefreshComplete();
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+
+                for (int i = 0; i < 5; i++) {
+                    StoreModel storeModel = new StoreModel();
+                    mListModel.add(storeModel);
+                }
+                mAdapter.notifyDataSetChanged();
+                mPtrlvContent.onRefreshComplete();
+            }
+        });
+//        mPtrlvContent.setRefreshing();
+    }
+
+    private void bindDefaultLvData() {
+        mAdapter = new MerchantListAdapter(mListModel, getActivity());
+        mPtrlvContent.setAdapter(mAdapter);
+    }
+
+    @Override
+    protected String setUmengAnalyticsTag() {
+        return this.getClass().getName().toString();
     }
 }
