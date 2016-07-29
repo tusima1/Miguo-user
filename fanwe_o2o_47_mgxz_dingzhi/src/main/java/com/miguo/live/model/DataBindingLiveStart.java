20e40cb8e27a3390ba47f65492902304db418d8e
cq <2639117470@qq.com>
2016-07-29 10:41:58 +0800

    开始直播，界面优化

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/live/model/DataBindingLiveStart.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/live/model/DataBindingLiveStart.java
@@ -1,16 +1,23 @@
 package com.miguo.live.model;
 
-import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
+import android.databinding.ObservableInt;
 
 
 public class DataBindingLiveStart {
 
-    public final ObservableBoolean isQQ = new ObservableBoolean();
-    public final ObservableBoolean isSina = new ObservableBoolean();
-    public final ObservableBoolean isWeiXin = new ObservableBoolean();
-    public final ObservableBoolean isFriend = new ObservableBoolean();
+    public final int NONE = 0;
+    public final int QQ = 1;
+    public final int WEIXIN = 2;
+    public final int FRIEND = 3;
+    public final int SINA = 4;
+
+
+    public final ObservableInt mode = new ObservableInt();
     //选择的店铺
     public final ObservableField<String> shopName = new ObservableField<String>();
 
+    public DataBindingLiveStart() {
+        mode.set(FRIEND);
+    }
 }
--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/live/views/LiveStartActivity.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/live/views/LiveStartActivity.java
@@ -3,9 +3,11 @@ package com.miguo.live.views;
 import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.fanwe.o2o.miguo.R;
-import com.fanwe.o2o.miguo.databinding.ActLiveBinding;
+import com.fanwe.o2o.miguo.databinding.ActLiveStartBinding;
 import com.miguo.live.model.DataBindingLiveStart;
 
 /**
@@ -17,10 +19,63 @@ public class LiveStartActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActLiveBinding binding = DataBindingUtil.setContentView(this, R.layout.act_live);
+        ActLiveStartBinding binding = DataBindingUtil.setContentView(this, R.layout.act_live_start);
         dataBindingLiveStart = new DataBindingLiveStart();
-        dataBindingLiveStart.shopName.set("请选择代言店铺");
+        dataBindingLiveStart.shopName.set("选择你的消费场所");
         binding.setLive(dataBindingLiveStart);
-        dataBindingLiveStart.shopName.set("我就是我 哈哈哈");
+    }
+
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.tv_shop_name:
+                dataBindingLiveStart.shopName.set("米果小站");
+                break;
+            case R.id.iv_qq_live_start:
+                if (dataBindingLiveStart.mode.get() == dataBindingLiveStart.QQ) {
+                    dataBindingLiveStart.mode.set(dataBindingLiveStart.NONE);
+                } else {
+                    dataBindingLiveStart.mode.set(dataBindingLiveStart.QQ);
+                }
+                break;
+            case R.id.iv_weixin_live_start:
+                if (dataBindingLiveStart.mode.get() == dataBindingLiveStart.WEIXIN) {
+                    dataBindingLiveStart.mode.set(dataBindingLiveStart.NONE);
+                } else {
+                    dataBindingLiveStart.mode.set(dataBindingLiveStart.WEIXIN);
+                }
+                break;
+            case R.id.iv_friend_live_start:
+                if (dataBindingLiveStart.mode.get() == dataBindingLiveStart.FRIEND) {
+                    dataBindingLiveStart.mode.set(dataBindingLiveStart.NONE);
+                } else {
+                    dataBindingLiveStart.mode.set(dataBindingLiveStart.FRIEND);
+                }
+                break;
+            case R.id.iv_sina_live_start:
+                if (dataBindingLiveStart.mode.get() == dataBindingLiveStart.SINA) {
+                    dataBindingLiveStart.mode.set(dataBindingLiveStart.NONE);
+                } else {
+                    dataBindingLiveStart.mode.set(dataBindingLiveStart.SINA);
+                }
+                break;
+            case R.id.btn_start_live_start:
+                startLive();
+                break;
+        }
+    }
+
+    private void startLive() {
+        if (dataBindingLiveStart.mode.get() == dataBindingLiveStart.QQ) {
+            Toast.makeText(this, "QQ", Toast.LENGTH_SHORT).show();
+        } else if (dataBindingLiveStart.mode.get() == dataBindingLiveStart.WEIXIN) {
+            Toast.makeText(this, "WEIXIN", Toast.LENGTH_SHORT).show();
+        } else if (dataBindingLiveStart.mode.get() == dataBindingLiveStart.FRIEND) {
+            Toast.makeText(this, "FRIEND", Toast.LENGTH_SHORT).show();
+        } else if (dataBindingLiveStart.mode.get() == dataBindingLiveStart.SINA) {
+            Toast.makeText(this, "SINA", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "NONE", Toast.LENGTH_SHORT).show();
+        }
     }
 }
