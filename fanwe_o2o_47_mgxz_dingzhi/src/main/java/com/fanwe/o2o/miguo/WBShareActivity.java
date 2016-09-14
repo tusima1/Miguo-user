4bbe631bd48af7ab5067eacf9d428c7c165cccf1
cq <2639117470@qq.com>
2016-09-14 11:27:09 +0800

    微信支付bug解决

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/o2o/miguo/WBShareActivity.java
+++
@@ -1,9 +0,0 @@
-package com.fanwe.o2o.miguo;
-
-import com.umeng.socialize.media.WBShareCallBackActivity;
-
-/**
- * Created by Administrator on 2016/7/26.
- */
-public class WBShareActivity extends WBShareCallBackActivity {
-}
