d5ca539a21781cc74b036ab5c2a39f9fa469a0ff
wwh <wg-dd@qq.com>
2016-10-10 09:20:28 +0800

    订单

---
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/common/AppUpdateManager.java
@@ -0,0 +1,9 @@
+package com.fanwe.common;
+
+/**
+ * Created by didik on 2016/10/10.
+ */
+
+public class AppUpdateManager {
+
+}
--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/user/adapters/OrderInAdapter.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/user/adapters/OrderInAdapter.java
@@ -141,7 +141,8 @@ public class OrderInAdapter extends SDBaseAdapter<ModelOrderItemIn> {
             if (TextUtils.isEmpty(str_dp_id)) {
                 dp_id = 0;
             } else {
-                dp_id = MGStringFormatter.getInt(str_dp_id);
+                //表示已经点评
+                dp_id = 1;
             }
 
             if ((mStatus_value == 3 || mStatus_value == 4) && dp_id == 0 && consume_count > 0) {
