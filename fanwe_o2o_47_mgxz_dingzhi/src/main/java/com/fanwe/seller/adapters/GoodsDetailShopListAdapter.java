e57450add08aa989a567f3cd1c4a45e7131e58a2
wwh <wg-dd@qq.com>
2016-10-21 14:27:22 +0800

    跳转百度地图

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/seller/adapters/GoodsDetailShopListAdapter.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/seller/adapters/GoodsDetailShopListAdapter.java
@@ -9,12 +9,16 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.fanwe.StoreLocationActivity;
 import com.fanwe.baidumap.BaiduMapManager;
+import com.fanwe.fragment.StoreLocationFragment;
 import com.fanwe.library.utils.SDActivityUtil;
 import com.fanwe.library.utils.SDIntentUtil;
+import com.fanwe.model.Store_infoModel;
 import com.fanwe.o2o.miguo.R;
 import com.fanwe.seller.model.getGroupDeatilNew.ShopListBean;
 import com.fanwe.seller.views.GoodsDetailActivity;
+import com.fanwe.utils.DataFormat;
 import com.fanwe.utils.SDDistanceUtil;
 import com.miguo.live.views.customviews.MGToast;
 
@@ -77,35 +81,63 @@ public class GoodsDetailShopListAdapter extends BaseAdapter {
             }else {
                 holder.tv_name_address.setText(shopListBean.getShop_name());
             }
-            int distanceFromMyLocation = (int) BaiduMapManager.getInstance()
-                    .getDistanceFromMyLocation(shopListBean.getGeo_x(), shopListBean.getGeo_y());
-            holder.tv_distance.setText(SDDistanceUtil.getKmDistanceString(distanceFromMyLocation));
+            String geo_x = shopListBean.getGeo_x();
+            String geo_y = shopListBean.getGeo_y();
+            if (TextUtils.isEmpty(geo_x) || TextUtils.isEmpty(geo_y)){
+                holder.iv_map.setImageResource(R.drawable.ic_map_disable);
+                holder.tv_distance.setText("--km");
+            }else {
+                holder.iv_map.setImageResource(R.drawable.ic_map_enable);
+                int distanceFromMyLocation = (int) BaiduMapManager.getInstance()
+                        .getDistanceFromMyLocation(geo_x,geo_y);
+                holder.tv_distance.setText(SDDistanceUtil.getKmDistanceString(distanceFromMyLocation));
+                holder.iv_map.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(parent.getContext(), StoreLocationActivity.class);
+                        Store_infoModel store_infoModel=new Store_infoModel();
+                        store_infoModel.setAddress(shopListBean.getAddress());
+                        store_infoModel.setXpoint(DataFormat.toDouble(shopListBean.getGeo_x()));
+                        store_infoModel.setYpoint(DataFormat.toDouble(shopListBean.getGeo_y()));
+                        store_infoModel.setId(shopListBean.getId());
+                        store_infoModel.setName(shopListBean.getShop_name());
+                        store_infoModel.setTel(shopListBean.getTel());
+
+                        intent.putExtra(StoreLocationFragment.EXTRA_MODEL_MERCHANTITEMACTMODEL, store_infoModel);
+                        SDActivityUtil.startActivity(mGoodsDetailActivity,intent);
+                    }
+                });
+            }
+
             holder.tv_time.setText(shopListBean.getTrade_day());
 
-            holder.iv_phone.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    String tel = shopListBean.getTel();
-                    if (!TextUtils.isEmpty(tel)){
+            final String tel = shopListBean.getTel();
+            if (TextUtils.isEmpty(tel)){
+                holder.iv_phone.setImageResource(R.drawable.ic_phone_disable);
+            }else {
+                holder.iv_phone.setImageResource(R.drawable.ic_phone_enable);
+                holder.iv_phone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
                         Intent intent = SDIntentUtil.getIntentCallPhone(tel);
                         SDActivityUtil.startActivity(mGoodsDetailActivity, intent);
                     }
-                }
-            });
+                });
+            }
+            String shop_id = shopListBean.getId();
+            if (TextUtils.isEmpty(shop_id)){
+                holder.iv_shop.setImageResource(R.drawable.ic_shop_disable);
+            }else {
+                holder.iv_shop.setImageResource(R.drawable.ic_shop_enable);
+                holder.iv_shop.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        MGToast.showToast("item:"+position+"去小店");
+                    }
+                });
+            }
 
-            holder.iv_map.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    MGToast.showToast("item:"+position+"应该去地图!");
-                }
-            });
 
-            holder.iv_shop.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    MGToast.showToast("item:"+position+"去小店");
-                }
-            });
         }
 
         return convertView;
