a31c2cd0169819a6a66ceeb2769a7a827d861331
cq <2639117470@qq.com>
2016-10-21 11:41:13 +0800

    冲突解决

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/adapter/barry/MainActivityHomeFragmentTuanAdapter.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/adapter/barry/MainActivityHomeFragmentTuanAdapter.java
@@ -61,14 +61,8 @@ public class MainActivityHomeFragmentTuanAdapter extends BarryBaseRecyclerAdapte
     @Override
     protected void doThings(RecyclerView.ViewHolder holder, int position) {
         setNormalPriceText(holder, position);
-<<<<<<< HEAD
-        getHolder(holder).freeReservation.setVisibility(TextUtils.isEmpty(getItem(position).getTuan_property_name().trim()) ? View.GONE : View.VISIBLE);
-
-=======
         getHolder(holder).freeReservation.setVisibility(TextUtils.isEmpty(trim(getItem(position).getTuan_property_name())) ? View.GONE : View.VISIBLE);
->>>>>>> remotes/origin/master
     }
-
 
 
     private void setNormalPriceText(RecyclerView.ViewHolder holder, int position) {
