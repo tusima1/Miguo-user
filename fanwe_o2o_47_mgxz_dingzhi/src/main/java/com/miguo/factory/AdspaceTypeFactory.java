0a1654fb088e60a34cd3b737de89bc1559d729aa
zlh <741367942@qq.com>
2016-11-04 10:06:16 +0800

    精选首页新交互完善 banner跳转类型完善 菜单跳转类型完善 更新4.4.4一下状态栏问题 修复首页下拉刷新和横向滚动滑动冲突太灵敏问题 首页精选添加标题 修复几个Activity跳转为反射获取 商家详情删除一些无用代码

---
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/factory/AdspaceTypeFactory.java
@@ -0,0 +1,7 @@
+package com.miguo.factory;
+
+/**
+ * Created by Administrator on 2016/11/2.
+ */
+public class AdspaceTypeFactory {
+}
---
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/listener/fragment/HomeBannerFragmentListener.java
@@ -0,0 +1,7 @@
+package com.miguo.listener.fragment;
+
+/**
+ * Created by Administrator on 2016/11/2.
+ */
+public class HomeBannerFragmentListener {
+}
