ee960429c72ee9e1edc85d148da5e1e2b276b517
zlh <741367942@qq.com>
2016-11-09 15:45:37 +0800

    修复首页刷新banner数据不及时显示，修复刷新后界面显示不友好，完善活动页跳转，修复精选团购两行显示重新计算高度

---
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/dao/ShoppingCartDao.java
@@ -0,0 +1,7 @@
+package com.miguo.dao;
+
+/**
+ * Created by Administrator on 2016/11/9.
+ */
+public interface ShoppingCartDao {
+}
---
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,7 @@
+package com.miguo.dao.impl;
+
+/**
+ * Created by Administrator on 2016/11/9.
+ */
+public class ShoppingCartDaoImpl {
+}
