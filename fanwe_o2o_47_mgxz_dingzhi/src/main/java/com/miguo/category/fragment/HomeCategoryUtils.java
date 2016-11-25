ae9f1aca6fe5ed10c132bb4fb2f0b653f565821f
zhhy <52022501@qq.com>
2016-11-25 20:55:53 +0800

    修改首页menu 里面的type跳转到商家列表时默认选择项的问题

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/category/fragment/HomeCategoryUtils.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/category/fragment/HomeCategoryUtils.java
@@ -14,20 +14,30 @@ public class HomeCategoryUtils {
 
     /**
      * 把JSON 解析成model
+     *
      * @param type_id
      * @return
      */
-    public static BannerTypeModel parseTypeJson(String type_id){
+    public static BannerTypeModel parseTypeJson(String type_id) {
         JSONObject resultJsonObject = null;
         BannerTypeModel model = new BannerTypeModel();
         try {
             resultJsonObject = new JSONObject(type_id);
-
-            model.setId(resultJsonObject.getString("id"));
-            model.setUrl(resultJsonObject.getString("url"));
-            model.setTag(resultJsonObject.getString("tag"));
-            model.setCate_id(resultJsonObject.getString(UserConstants.CATE_ID));
-            model.setTid(resultJsonObject.getString(UserConstants.TID));
+            if (resultJsonObject.has("id")) {
+                model.setId(resultJsonObject.getString("id"));
+            }
+            if (resultJsonObject.has("url")) {
+                model.setUrl(resultJsonObject.getString("url"));
+            }
+            if (resultJsonObject.has("tag")) {
+                model.setTag(resultJsonObject.getString("tag"));
+            }
+            if (resultJsonObject.has(UserConstants.CATE_ID)) {
+                model.setCate_id(resultJsonObject.getString(UserConstants.CATE_ID));
+            }
+            if (resultJsonObject.has(UserConstants.TID)) {
+                model.setTid(resultJsonObject.getString(UserConstants.TID));
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
