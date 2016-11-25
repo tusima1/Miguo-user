7ba1c1532407aa5e3ffe43bcb36e84a8acc321da
wwh <wg-dd@qq.com>
2016-11-25 14:59:23 +0800

    统一的遮罩

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/seller/adapters/GoodsDetailPagerAdapter.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/seller/adapters/GoodsDetailPagerAdapter.java
@@ -1,11 +1,15 @@
 package com.fanwe.seller.adapters;
 
 import android.support.v4.view.PagerAdapter;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
+import com.fanwe.customview.MGGradientView;
 import com.fanwe.library.utils.SDViewBinder;
+import com.miguo.utils.DisplayUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,10 +43,25 @@ public class GoodsDetailPagerAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
+        FrameLayout frameLayout=new FrameLayout(container.getContext());
+
         ImageView imageView = new ImageView(container.getContext());
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
         SDViewBinder.setImageView(mData.get(position),imageView);
-        container.addView(imageView);
-        return imageView;
+        frameLayout.addView(imageView);
+
+        MGGradientView gradient=new MGGradientView(container.getContext());
+        int height = DisplayUtil.dp2px(container.getContext(), 100);
+//        int margin = DisplayUtil.dp2px(container.getContext(), 24);
+        FrameLayout.LayoutParams params=new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,height);
+        params.gravity= Gravity.TOP;
+//        params.setMargins(0,margin,0,0);
+        gradient.setGravity(MGGradientView.TOP);
+        gradient.setLayoutParams(params);
+        frameLayout.addView(gradient);
+
+        container.addView(frameLayout);
+
+        return frameLayout;
     }
 }
