b6b02928ac8a12419368b4c0d1deb8b71ea6d7eb
cq <2639117470@qq.com>
2016-12-15 17:19:54 +0800

    6.0.40测试版本

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/constant/ServerUrl.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/constant/ServerUrl.java
@@ -6,30 +6,41 @@ package com.fanwe.constant;
  * @author Administrator
  */
 public class ServerUrl {
-    /***************************!!!上线需要修改模式!!!****************************/
+    /***************************
+     * !!!上线需要修改模式!!!
+     ****************************/
     public static final boolean DEBUG = true;//默认开启debug,上线改为false即可,其他接口API不要改动
 
-    /*************************打测试包******************************/
-    public static final boolean TEST = false;//给测试使用请设置为true
+    /*************************
+     * 打测试包
+     ******************************/
+    public static final boolean TEST = true;//给测试使用请设置为true
 
-    /*********************** Do Not Modify ***********************/
+    /***********************
+     * Do Not Modify
+     ***********************/
     public static final String KEY_AES = "FANWE5LMUQC436IM";
     private static final String SERVER_API_URL_ONLINE = "http://mapi.mgxz.com";
-    private static final String SERVER_H5_ONLINE="http://m.mgxz.com/";//线上
-    /*********************** Do Not Modify ***********************/
-    private static String SERVER_H5_USING =SERVER_H5_ONLINE;
-    private static String SERVER_API_USING=SERVER_API_URL_ONLINE;
+    private static final String SERVER_H5_ONLINE = "http://m.mgxz.com/";//线上
+    /***********************
+     * Do Not Modify
+     ***********************/
+    private static String SERVER_H5_USING = SERVER_H5_ONLINE;
+    private static String SERVER_API_USING = SERVER_API_URL_ONLINE;
 
-    public static void setServerApi(String api){
-        if (!DEBUG)return;
-        SERVER_API_USING=api;
-    }
-    public static void setServerH5Using(String h5api){
-        if (!DEBUG)return;
-        SERVER_H5_USING =h5api;
+    public static void setServerApi(String api) {
+        if (!DEBUG) return;
+        SERVER_API_USING = api;
     }
 
-    /************* Add End ************/
+    public static void setServerH5Using(String h5api) {
+        if (!DEBUG) return;
+        SERVER_H5_USING = h5api;
+    }
+
+    /*************
+     * Add End
+     ************/
 
     public static String getAppServerApiUrl() {
         if (DEBUG) {
@@ -40,10 +51,10 @@ public class ServerUrl {
         }
     }
 
-    public static String getAppH5Url(){
-        if (DEBUG){
+    public static String getAppH5Url() {
+        if (DEBUG) {
             return SERVER_H5_USING;
-        }else {
+        } else {
             return SERVER_H5_ONLINE;
         }
     }
