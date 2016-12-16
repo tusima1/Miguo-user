73484024ad699b03665d11c25660799fb6a0c04b
wwh <wg-dd@qq.com>
2016-12-16 20:56:28 +0800

    我的战队第一次数据重复展示

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/fragment/MyDistFragment.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/fragment/MyDistFragment.java
@@ -1,6 +1,8 @@
 package com.fanwe.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +14,9 @@ import com.fanwe.adapter.MyXiaoMiAdapter;
 import com.fanwe.base.CallbackView;
 import com.fanwe.customview.SDListViewInScroll;
 import com.fanwe.library.utils.SDCollectionUtil;
+import com.fanwe.library.utils.SDViewBinder;
 import com.fanwe.model.Member;
+import com.fanwe.model.PageModel;
 import com.fanwe.o2o.miguo.R;
 import com.fanwe.user.UserConstants;
 import com.fanwe.user.model.getMyDistributionCorps.ModelMyDistributionCorps;
@@ -24,7 +28,6 @@ import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener2;
 import com.handmark.pulltorefresh.library.PullToRefreshScrollView;
 import com.lidroid.xutils.view.annotation.ViewInject;
-import com.miguo.utils.MGUIUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,16 +61,16 @@ public class MyDistFragment extends BaseFragment implements View.OnClickListener
     private TextView mTv_vip2Number;
 
     private MyXiaoMiAdapter mAdapter;
-
+    private PageModel mPage = new PageModel();
     private List<Member> listModel = new ArrayList<Member>();
     protected OnDialogData mListener;
 
     private int mType;
     private String mRank;
     private int pageNum = 1;
-    private  int maxPage=1;
     private int pageSize = 10;
     private UserHttpHelper userHttpHelper;
+    private boolean isRefresh = true;
 
     public void setmListener(OnDialogData listener) {
         this.mListener = listener;
@@ -86,10 +89,15 @@ public class MyDistFragment extends BaseFragment implements View.OnClickListener
         initClick();
         initPullToScrollView();
     }
+
     public void setPageType(int pageType) {
         this.mType = pageType;
     }
+
     public void clearBtn() {
+        mRank = "";
+        pageNum = 1;
+        isRefresh = true;
         mLl_vip1.setEnabled(true);
         mLl_vip2.setEnabled(true);
         mTv_textVip1.setTextColor(getResources().getColor(R.color.text_fenxiao));
@@ -103,145 +111,24 @@ public class MyDistFragment extends BaseFragment implements View.OnClickListener
         userHttpHelper.getMyDistributionCorps(mType + "", mRank, pageNum, pageSize, "");
     }
 
+    private ResultMyDistributionCorps currResultMyDistributionCorps;
 
-    public void finishRefresh(){
-        mPtr_ScrollView.onRefreshComplete();
-    }
-
-    public void setResultMyDistributionCorps(ResultMyDistributionCorps  datas) {
-        finishRefresh();
-
-        parseData(datas);
-    }
-
-
-
-    private void initClick() {
-        mLl_vip1.setOnClickListener(this);
-        mLl_vip2.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.ll_vip1:
-                clickVip1();
-                break;
-            case R.id.ll_vip2:
-                clickVip2();
-                break;
-            default:
-                break;
+    public void setResultMyDistributionCorps(ResultMyDistributionCorps bean) {
+        this.currResultMyDistributionCorps = bean;
+        this.results.add(bean);
+        clearBtn();
+        if (currResultMyDistributionCorps != null) {
+            SDViewBinder.setTextView(mTv_vip1Number, "（" + currResultMyDistributionCorps.getLevel1() + "）");
+            SDViewBinder.setTextView(mTv_vip2Number, "（" + currResultMyDistributionCorps.getLevel2() + "）");
+            convertList();
         }
     }
 
-    private void clickVip2() {
-        mRank = "2";
-
-        pageNum = 1;
-        mLl_vip1.setEnabled(true);
-        mLl_vip2.setEnabled(false);
-        mTv_textVip1
-                .setTextColor(getResources().getColor(R.color.text_fenxiao));
-        mTv_textVip2.setTextColor(getResources().getColor(R.color.main_color));
-        getData();
-    }
-
-    private void clickVip1() {
-        mRank = "1";
-
-        pageNum = 1;
-        mLl_vip1.setEnabled(false);
-        mLl_vip2.setEnabled(true);
-        mTv_textVip1.setTextColor(getResources().getColor(R.color.main_color));
-        mTv_textVip2
-                .setTextColor(getResources().getColor(R.color.text_fenxiao));
-        getData();
-    }
-
-    private void initPullToScrollView() {
-        mPtr_ScrollView.setMode(Mode.BOTH);
-        mPtr_ScrollView
-                .setOnRefreshListener(new OnRefreshListener2<ScrollView>() {
-                    @Override
-                    public void onPullDownToRefresh(
-                            PullToRefreshBase<ScrollView> refreshView) {
-                        pageNum = 1;
-                        mLl_vip1.setEnabled(true);
-                        mLl_vip2.setEnabled(true);
-                        mTv_textVip1.setTextColor(getResources().getColor(
-                                R.color.text_fenxiao));
-                        mTv_textVip2.setTextColor(getResources().getColor(
-                                R.color.text_fenxiao));
-                        getData();
-                    }
-
-                    @Override
-                    public void onPullUpToRefresh(
-                            PullToRefreshBase<ScrollView> refreshView) {
-                       if(maxPage>=pageNum) {
-                           getData();
-                       }
-                    }
-                });
-//      mPtr_ScrollView.setRefreshing();
-    }
-
-    private void bindData() {
-        mAdapter = new MyXiaoMiAdapter(listModel, getActivity(), mType);
-        mListView.setAdapter(mAdapter);
-        mListView.setEmptyView(mLl_empty);
-    }
-
-    @Override
-    public void onSuccess(String responseBody) {
-
-    }
-
-
-    @Override
-    public void onSuccess(String method, final List datas) {
-        finishRefresh();
-        switch (method){
-            case UserConstants.MY_DISTRIBUTION_CROPS:
-                MGUIUtil.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        if(datas==null){
-                            return;
-                        }
-                        parseData((ResultMyDistributionCorps)datas.get(0));
-                    }
-                });
-                break;
-            default:
-                break;
-
-        }
-
-    }
-
-    /**
-     * 解析数据。
-     * @param datas
-     */
-    public void parseData(ResultMyDistributionCorps datas){
-        if(datas==null){
-            return;
-        }
-        com.miguo.live.model.PageModel mPage =  datas.getPage();
-        if(mPage ==null){
-            return;
-        }
-        pageNum = Integer.valueOf(mPage.getPage());
-        maxPage = Integer.valueOf(mPage.getPage_total());
-        if(pageNum==1){
+    private void convertList() {
+        if (isRefresh)
             listModel.clear();
-        }
-        pageNum ++;
-
-        if (!SDCollectionUtil.isEmpty(datas.getList())) {
-            for (ModelMyDistributionCorps bean : datas.getList()) {
+        if (!SDCollectionUtil.isEmpty(currResultMyDistributionCorps.getList())) {
+            for (ModelMyDistributionCorps bean : currResultMyDistributionCorps.getList()) {
                 Member member = new Member();
                 member.setId(bean.getUser_id());
                 member.setUid(bean.getUser_id());
@@ -259,17 +146,130 @@ public class MyDistFragment extends BaseFragment implements View.OnClickListener
         mAdapter.notifyDataSetChanged();
     }
 
+    private void initClick() {
+        mLl_vip1.setOnClickListener(this);
+        mLl_vip2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        mPage.resetPage();
+        switch (v.getId()) {
+            case R.id.ll_vip1:
+                clickVip1();
+                break;
+            case R.id.ll_vip2:
+                clickVip2();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void clickVip2() {
+        mRank = "2";
+        isRefresh = true;
+        pageNum = 1;
+        mLl_vip1.setEnabled(true);
+        mLl_vip2.setEnabled(false);
+        mTv_textVip1
+                .setTextColor(getResources().getColor(R.color.text_fenxiao));
+        mTv_textVip2.setTextColor(getResources().getColor(R.color.main_color));
+        getData();
+    }
+
+    private void clickVip1() {
+        mRank = "1";
+        isRefresh = true;
+        pageNum = 1;
+        mLl_vip1.setEnabled(false);
+        mLl_vip2.setEnabled(true);
+        mTv_textVip1.setTextColor(getResources().getColor(R.color.main_color));
+        mTv_textVip2
+                .setTextColor(getResources().getColor(R.color.text_fenxiao));
+        getData();
+    }
+
+    private void initPullToScrollView() {
+        mPtr_ScrollView.setMode(Mode.BOTH);
+        mPtr_ScrollView
+                .setOnRefreshListener(new OnRefreshListener2<ScrollView>() {
+                    @Override
+                    public void onPullDownToRefresh(
+                            PullToRefreshBase<ScrollView> refreshView) {
+                        isRefresh = true;
+                        pageNum = 1;
+                        mLl_vip1.setEnabled(true);
+                        mLl_vip2.setEnabled(true);
+                        mTv_textVip1.setTextColor(getResources().getColor(
+                                R.color.text_fenxiao));
+                        mTv_textVip2.setTextColor(getResources().getColor(
+                                R.color.text_fenxiao));
+                        getData();
+                    }
+
+                    @Override
+                    public void onPullUpToRefresh(
+                            PullToRefreshBase<ScrollView> refreshView) {
+                        isRefresh = false;
+                        if (!SDCollectionUtil.isEmpty(results)) {
+                            pageNum++;
+                        }
+                        getData();
+                    }
+                });
+//        mPtr_ScrollView.setRefreshing();
+    }
+
+    private void bindData() {
+        mAdapter = new MyXiaoMiAdapter(listModel, getActivity(), mType);
+        mListView.setAdapter(mAdapter);
+        mListView.setEmptyView(mLl_empty);
+    }
+
+    @Override
+    public void onSuccess(String responseBody) {
+
+    }
+
+    List<ResultMyDistributionCorps> results=new ArrayList<>();
+    ResultMyDistributionCorps currResult;
+
+    @Override
+    public void onSuccess(String method, List datas) {
+        Message msg = new Message();
+        if (UserConstants.MY_DISTRIBUTION_CROPS.equals(method)) {
+            results = datas;
+            msg.what = 0;
+        }
+        mHandler.sendMessage(msg);
+    }
+
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 0:
+                    if (!SDCollectionUtil.isEmpty(results)) {
+                        currResult = results.get(0);
+                        currResultMyDistributionCorps = currResult;
+                        convertList();
+                        mPtr_ScrollView.onRefreshComplete();
+                    }
+                    break;
+            }
+        }
+    };
 
 
     @Override
     public void onFailue(String responseBody) {
-        finishRefresh();
 
     }
 
     @Override
     public void onFinish(String method) {
-        mPtr_ScrollView.onRefreshComplete();
 
     }
 
