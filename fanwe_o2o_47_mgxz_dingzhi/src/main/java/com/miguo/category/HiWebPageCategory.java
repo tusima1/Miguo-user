9dcc1846d16f363614004f326bf8731e0a4f4343
zhhy <52022501@qq.com>
2016-12-21 10:57:56 +0800

    修改我的奖品跳转 问题

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/category/HiWebPageCategory.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/category/HiWebPageCategory.java
@@ -156,7 +156,7 @@ public class HiWebPageCategory extends Category implements ShoppingCartView {
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 //  重写此方法表明点击网页里面的链接还是在当前的webview里跳转，不跳到浏览器那边
                 view.loadUrl(url);
-                return true;
+                return false;
             }
 
             @Override
--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/ui/view/AutoBanner.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/ui/view/AutoBanner.java
@@ -1,20 +1,17 @@
 package com.miguo.ui.view;
 
-import android.graphics.Color;
-import android.support.v4.view.LoopViewPager;
-import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.widget.RelativeLayout;
 import android.content.Context;
 import android.database.DataSetObserver;
+import android.support.v4.view.LoopViewPager;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 
 import com.fanwe.o2o.miguo.R;
 import com.miguo.live.views.base.BaseRelativeLayout;
@@ -79,7 +76,7 @@ public class AutoBanner extends BaseRelativeLayout implements ViewPager.OnPageCh
         barHight = dip2px(8);
         pager = new LoopViewPager(getContext());
         pager.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
-        pager.setId(0x123456);
+        pager.setId(R.id.aaaaaa);
         pager.setOnPageChangeListener(this);
         addView(pager);
 
