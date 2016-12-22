35b7e929705ae427cf5be72e22d323bee131107e
cq <2639117470@qq.com>
2016-12-22 16:58:33 +0800

    用户资金日志 接口更新

--- fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/commission/model/CommissionConstance.java
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/fanwe/commission/model/CommissionConstance.java
@@ -8,28 +8,30 @@ public class CommissionConstance {
     /**
      * 资金日志
      */
-    public static final String COMMISSION_LOG="CommissionLog";
+    public static final String COMMISSION_LOG = "CommissionLog";
     /**
      * 获取用户的信息(比较全)
      */
-    public static final String USER_ACCOUNT="UserAccount";
+    public static final String USER_ACCOUNT = "UserAccount";
     /**
      *
      */
-    public static final String USER_WITHDRAW_LOG="UserWithdrawLog";
+    public static final String USER_WITHDRAW_LOG = "UserWithdrawLog";
 
     /**
      * 余额提现
      */
-    public static final String USER_WITHDRAW="UserBalanceWithdraw";
+    public static final String USER_WITHDRAW = "UserBalanceWithdraw";
     /**
      * 佣金提现
      */
-    public static final String USER_WITHDRAW_FX="UserCommissionWithdraw";
+    public static final String USER_WITHDRAW_FX = "UserCommissionWithdraw";
     /**
      * 银行卡列表
      */
-    public static final String USER_BANK_CARD_LIST="UserBankCardList";
-    /*用户资金日志*/
-    public static final String USER_COMMISSION_LOG="UserCommissionLog";
+    public static final String USER_BANK_CARD_LIST = "UserBankCardList";
+    /**
+     * 用户资金日志
+     */
+    public static final String USER_COMMISSION_LOG = "UserCommissionLogNew";
 }
