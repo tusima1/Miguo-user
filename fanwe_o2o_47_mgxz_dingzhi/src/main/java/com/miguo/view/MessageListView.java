fb7035d1211862effb8cfa59d05c40c7a73e4e69
zlh <741367942@qq.com>
2017-03-20 18:20:33 +0800

    二级消息列表对接

---
+++ fanwe_o2o_47_mgxz_dingzhi/src/main/java/com/miguo/view/MessageListView.java
@@ -0,0 +1,25 @@
+package com.miguo.view;
+
+import com.miguo.entity.MessageListBean;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/3/20.
+ */
+
+public interface MessageListView extends BaseView {
+
+    /**
+     * 获取消息列表成功
+     * @param messages 消息列表
+     */
+    void getMessageListSuccess(List<MessageListBean.Result.Body> messages);
+
+    /**
+     * 获取消息列表失败
+     * @param message 失败回调消息
+     */
+    void getMessageListError(String message);
+
+}
